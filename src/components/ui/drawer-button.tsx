d6e465fde6993ca576e2236733cbbc8966de1e8e
NibanK <nabin.kawan.9@gmail.com>
2023-02-20 15:38:24 +0545

    Add drawer button

---
+++ src/components/ui/drawer-button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDrawer } from '../drawer/context';
+import { AiOutlineMenu } from 'react-icons/ai';
+
+export default function DrawerButton() {
+  const { isOpen, openDrawer, closeDrawer } = useDrawer();
+
+  const handleDrawer = () => {
+    if (isOpen) {
+      closeDrawer();
+    } else {
+      openDrawer();
+    }
+  };
+  return (
+    <div
+      className="group inline-flex lg:hidden rounded-full p-3 bg-gray-200  hover:bg-blue-400 cursor-pointer mb-4"
+      onClick={handleDrawer}
+    >
+      <AiOutlineMenu className="group-hover:text-white" size={18} />
+    </div>
+  );
+}
