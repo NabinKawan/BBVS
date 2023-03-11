67f5600612fd99be1568ce1665e303cd8db651c3
NibanK <nabin.kawan.9@gmail.com>
2023-03-11 13:34:33 +0545

    Change abi & contract addr

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -2,5 +2,5 @@ export const OwnerContractAddrs = '0x2A36978f1ad81BFA5fA2c63556a6995E9A94c691';
 
 export const StorageContractAddrs = '0xA8388dCb0eBcf24cF1B50eCEc2826c39ffF547c3';
 
-export const ElectionContractAddrs = '0x4E3697f8bac439D6Ab54cBBa5d9e6950dF5Fbbbe';
+export const ElectionContractAddrs = '0x04B7d2Ed7d8FE873Ed491d2e353C1C9468C59367';
 // 0x29FE64dE5E5e1c25b1673058EF6a1c0aE27847b9
