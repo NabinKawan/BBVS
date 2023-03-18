7e0f85e07c6fbc27a7cb88f595fce2a8dc452c58
NibanK <nabin.kawan.9@gmail.com>
2023-03-18 13:00:41 +0545

    Add docker file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+  },
   swcMinify: true,
   publicRuntimeConfig: {
     BBVS_API_URL: process.env.BBVS_API_URL,
